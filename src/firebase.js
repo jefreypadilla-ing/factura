f8f4af2a0c3b5b8ec63c42308fa381081d7585e6
Jefrey Padilla <ing_jefreypadilla@hotmail.com>
2021-03-19 10:50:56 -0500

    Load react-dotenv

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore';
+import env from "react-dotenv";
 
 var firebaseConfig = {
-    apiKey: process.env.API_KEY,
+    apiKey: env.API_KEY,
     authDomain: "bluefarmamjpos.firebaseapp.com",
     databaseURL: "https://react-firebase-crud-2d065.firebaseio.com",
-    projectId: process.env.PROJECT_ID,
+    projectId: env.PROJECT_ID,
     storageBucket: "react-firebase-crud-2d065.appspot.com",
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
+    messagingSenderId: env.MESSAGING_SENDER_ID,
+    appId: env.APP_ID,
     measurementId: "G-P307XV6FHD"
 };
 // Initialize Firebase
