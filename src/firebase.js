692a4909e96e01fe38811af7ebf720d1473daf35
Jefrey Padilla <ing_jefreypadilla@hotmail.com>
2021-03-19 10:15:44 -0500

    Setting enviroment variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app'
 import 'firebase/firestore';
 
 var firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: process.env.API_KEY,
+    authDomain: "bluefarmamjpos.firebaseapp.com",
+    databaseURL: "https://react-firebase-crud-2d065.firebaseio.com",
+    projectId: process.env.PROJECT_ID,
+    storageBucket: "react-firebase-crud-2d065.appspot.com",
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: "G-P307XV6FHD"
 };
 // Initialize Firebase
 const fb = firebase.initializeApp(firebaseConfig);
